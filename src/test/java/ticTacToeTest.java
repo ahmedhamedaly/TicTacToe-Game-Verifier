ec7b76bdd48646bd9bcc48cf09f2d622a86082cf
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-22 16:06:52 +0000

    constructor test

--- src/test/java/ticTacToeTest.java
+++ src/test/java/ticTacToeTest.java
@@ -1,6 +1,6 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
+import java.util.Arrays;
 import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
@@ -8,7 +8,23 @@ public class ticTacToeTest {
 
     @org.junit.Test
     public void constructor() {
+        char[][] board = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
 
+        System.out.println(
+                "\n-Running Constructor-\n\n" +
+                "Input board: \n" +
+                Arrays.deepToString(board)
+        );
+
+        new ticTacToe(board);
+
+        System.out.println(
+                "Success!\n"
+        );
     }
 
     @org.junit.Test

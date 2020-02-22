822107a36313104257aaf0ed5236f3c13524f6f7
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-22 15:47:35 +0000

    added constructor

--- src/main/java/ticTacToe.java
+++ src/main/java/ticTacToe.java
@@ -1,2 +1,19 @@
 public class ticTacToe {
+
+    // piece states: noughts ('O'), crosses ('X') and blank (' ')
+    public static final char NOUGHTS = 'O';
+    public static final char CROSSES = 'X';
+    public static final char BLANK = ' ';
+
+    // board[row][col]
+    char[][] board;
+
+    /**
+     * CONSTRUCTOR
+     * @PARAMETERS: char[row][col] board
+     */
+    public ticTacToe(char[][] board) {
+        this.board = board;
+    }
+
 }

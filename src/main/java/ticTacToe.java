f8ad8634f4530c234973a9bf196270792d9dcbc6
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-22 15:56:26 +0000

    added game state which decided if the player won or not

--- src/main/java/ticTacToe.java
+++ src/main/java/ticTacToe.java
@@ -16,4 +16,37 @@ public class ticTacToe {
         this.board = board;
     }
 
+    /**
+     * STATE
+     * @PARAMETERS: None
+     * @RETURNS: char player
+     */
+    public char state() {
+        if (isWinner(NOUGHTS))
+            return NOUGHTS;
+        else if (isWinner(CROSSES))
+            return CROSSES;
+        else
+            return BLANK;
+    }
+
+    /**
+     * IS WINNER
+     * @PARAMETERS: char player
+     * @RETURNS: boolean win
+     */
+    public boolean isWinner(char player) {
+        return  // HORIZONTAL CHECK
+                board[0][0] == player && board[0][1] == player && board[0][2] == player ||
+                        board[1][0] == player && board[1][1] == player && board[1][2] == player ||
+                        board[2][0] == player && board[2][1] == player && board[2][2] == player ||
+                        // VERTICAL CHECK
+                        board[0][0] == player && board[1][0] == player && board[2][0] == player ||
+                        board[0][1] == player && board[1][1] == player && board[2][1] == player ||
+                        board[0][2] == player && board[1][2] == player && board[2][2] == player ||
+                        // DIAGONAL CHECK
+                        board[0][0] == player && board[1][1] == player && board[2][2] == player ||
+                        board[0][2] == player && board[1][1] == player && board[2][0] == player;
+    }
+
 }

a547d55202b245a63d0eb9f155e0200aa5a48216
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-22 16:00:18 +0000

    added maven junit dependency and structure for tests

--- src/test/java/ticTacToeTest.java
+++ src/test/java/ticTacToeTest.java
@@ -1,2 +1,23 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
 public class ticTacToeTest {
+
+    @org.junit.Test
+    public void constructor() {
+
+    }
+
+    @org.junit.Test
+    public void state() {
+
+    }
+
+    @org.junit.Test
+    public void isWinner() {
+
+    }
 }

450bd0637ce53dac44aa95e6e7c61986cb1beb74
Ahmed <ahmedhamedaly19@gmail.com>
2020-02-22 16:31:40 +0000

    created tests for state and isWinner

--- src/test/java/ticTacToeTest.java
+++ src/test/java/ticTacToeTest.java
@@ -29,11 +29,102 @@ public class ticTacToeTest {
 
     @org.junit.Test
     public void state() {
+        char expected;
 
+        char[][] board = {
+                {'X', 'O', 'O'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+
+        System.out.println(
+                "\n-Running State-\n\n" +
+                "Input board: \n" +
+                Arrays.deepToString(board)
+        );
+        expected = 'X';
+
+        ticTacToe game = new ticTacToe(board);
+        assertEquals(expected, game.state());
+
+        System.out.println(
+                "Expected: " + expected + "\n" +
+                "Success!" + "\n"
+        );
+
+        board = new char[][]{
+                {'O', 'O', 'O'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+
+        expected = 'O';
+
+        game = new ticTacToe(board);
+        assertEquals(expected, game.state());
+
+        System.out.println(
+                "Input board: \n" +
+                Arrays.deepToString(board) + "\n" +
+                "Expected: " + expected + "\n" +
+                "Success!" + "\n"
+        );
+
+        board = new char[][]{
+                {' ', 'O', 'O'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+
+        expected = ' ';
+
+        game = new ticTacToe(board);
+        assertEquals(expected, game.state());
+
+        System.out.println(
+                "Input board: \n" +
+                Arrays.deepToString(board) + "\n" +
+                "Expected: " + expected + "\n" +
+                "Success!" + "\n"
+        );
     }
 
     @org.junit.Test
     public void isWinner() {
+        char[][] board = {
+                {'O', 'O', 'O'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
 
+        System.out.println(
+                "\n-Running isWinner-\n\n" +
+                "Input board: \n" +
+                Arrays.deepToString(board)
+        );
+
+        ticTacToe game = new ticTacToe(board);
+        assertTrue(game.isWinner('O'));
+
+        System.out.println(
+                "Expected: true\n" +
+                "Success!\n"
+        );
+
+        board = new char[][]{
+                {'X', 'O', 'O'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'}
+        };
+
+        game = new ticTacToe(board);
+        assertTrue(game.isWinner('X'));
+
+        System.out.println(
+                "Input board: \n" +
+                Arrays.deepToString(board) + "\n" +
+                "Expected: true\n" +
+                "Success!\n"
+        );
     }
 }
